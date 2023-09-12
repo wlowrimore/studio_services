a1e082eceb73d147c2c224b221663b7a12e2004d
William Lowrimore <wlowrimore@gmail.com>
2023-09-12 01:58:35 -0500

    corrects the w, h and margins on homepage

--- app/components/home/page.jsx
+++ app/components/home/page.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
     // <div className="md:min-h-screen w-screen bg-customhome bg-fixed bg-no-repeat bg-cover bg-center flex flex-col items-center overflow-y-auto">
     <div className="w-screen bg-customhome bg-fixed bg-no-repeat bg-cover bg-center">
       {/* <div className="flex flex-col justify-center w-full h-[30rem] border-t-4 border-white mx-auto bg-gray-950 bg-opacity-80 py-12 pl-[20rem] pr-[15rem]"> */}
-      <div className="flex flex-col items-center md:items-start w-full c border-y-4 border-white mx-auto px-2 py-4 md:py-12 bg-gray-950 bg-opacity-60 overflow-y-auto">
+      <div className="flex flex-col items-center md:items-start w-full c border-y-4 border-white mx-auto px-2 py-4 bg-gray-950 md:py-12 md:h-[40rem] md:pl-[20rem] md:pr-[15rem] bg-opacity-60 overflow-y-auto">
         <h1 className="text-xl md:text-3xl text-white tracking-wider uppercase font-semibold">
           A Unique Recording Experience
         </h1>
